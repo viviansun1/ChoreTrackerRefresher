3a9a909eb8608342eeea2fcb5e14bc618bdddf66
viviansun1 <vivian.sun777@gmail.com>
2021-02-07 12:40:44 -0500

    added chore model and tests, all pass

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,20 @@
 class Chore < ApplicationRecord
-    belongs_to :child
-    belongs_to :tasks
+  belongs_to :child
+  belongs_to :task
+
+  # Validations
+  validates_date :due_on
+  
+  # Scopes
+  scope :by_task, -> { joins(:task).order('tasks.name') }
+  scope :chronological, -> { order(due_on: :asc, completed: :asc) }
+  scope :done, -> { where(completed: true) }
+  scope :pending, -> { where(completed: false) }
+  scope :upcoming, -> { where('due_on >= ?', Date.today) }
+  scope :past, -> { where('due_on < ?', Date.today) }
+  
+  # Other methods
+  def status
+    completed ? "Completed" : "Pending"
+  end
 end
